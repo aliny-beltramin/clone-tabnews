30be7123ee65cb0e2b4e5e98e447b7b780f64984
aliny-beltramin <beltraliny@gmail.com>
2025-04-29 11:41:13 +0000

    add scripts `lint:check` e  `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Esse pedaço da internet ainda é mato :D</h1>;
+  return <h1>Esse pedaço da internet ainda é mato :D</h1>;
+}
+
+function test() {
+  const teste = "rest";
 }
 
 export default Home;
